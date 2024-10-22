b6428c2a163c27d587f967cbb1eaf5f5c39484e7
Jacob0521GC <200601912@student.georgianc.on.ca>
2024-10-22 18:21:06 -0400

    Pong Scene Added
    - Starter scene removed and replaced with PongScene.
    
    - In PongScene the sprites Ball, Player1, Player2 where added and stored together under Paddles & Ball
    
    - main-script Changed to MainScript (Name was Causing an error)

--- Assets/Scenes/Scripts/MainScript.cs
+++ Assets/Scenes/Scripts/MainScript.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class main-script : MonoBehaviour
+public class MainScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
