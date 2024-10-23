ff2edb5140644a5a553f47a5a6f05bb596371266
Evan Gentile <ewgentile@gmail.com>
2024-10-23 14:25:27 -0400

    added 2 player movement

---
+++ Assets/Scenes/Scripts/Scoring.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Scoring : MonoBehaviour
+{
+    // Two seperate scoring integers
+    public int playerOneScore = 0;
+    public int playerTwoScore = 0;
+
+    // Ball game object reference
+    public GameObject Ball;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if GameObject.Find (Ball).transform.x => 10.0f();
+            {
+                
+            }
+
+        
+    }
+}
