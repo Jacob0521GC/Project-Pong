9e73596d4696dd098fc6caf07d71b024bbc1b988
Jacob0521GC <200601912@student.georgianc.on.ca>
2024-10-23 17:25:01 -0400

    Ball script and background
    - MainScript.cs removed and replaced with Ball.cs
    
    - Pong ball now moves and Bounces off the top and bottom of the screen
    
    - Background design added

---
+++ Assets/Scenes/Scripts/Ball.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    Vector2 directionX = Vector2.right;
+    Vector2 directionY = Vector2.up;
+    float speed = 3.0f;
+
+    void Start()
+    {
+     
+    }
+
+    void Update()
+    {
+        float dt = Time.deltaTime;
+        Vector3 changeX = directionX * speed * dt;
+        Vector3 changeY = directionY * speed * dt;
+        transform.position = transform.position + changeX + changeY;
+
+        // If the ball hits the top it bounces off
+        if (transform.position.y > 4.4f)
+        {
+            directionY = -directionY;
+        }
+
+        // If the ball hits the bottom it bounces off
+        if (transform.position.y < -4.4f)
+        {
+            directionY = -directionY;
+        }
+
+
+        if (transform.position.x > 8.7f)
+        {
+            
+        }
+
+
+        if (transform.position.x < -8.7f)
+        {
+            
+        }
+
+    }
+}
